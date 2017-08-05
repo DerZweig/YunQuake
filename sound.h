560faa2fcf4bfb2ff938ab42ff71e3eafa5869b2
Alex <info@broodtech.de>
2017-08-05 12:35:14 +0200

    -sound cleanup

--- sound.h
+++ sound.h
@@ -1,25 +1,3 @@
-/*
-Copyright (C) 1996-2001 Id Software, Inc.
-Copyright (C) 2002-2009 John Fitzgibbons and others
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-*/
-// sound.h -- client sound i/o functions
-
 #ifndef __SOUND__
 #define __SOUND__
 
@@ -31,78 +9,78 @@ struct portable_samplepair_t
 {
 	int left;
 	int right;
-} ;
+};
 
 struct sfx_t
 {
-	char 	name[MAX_QPATH];
-	cache_user_t	cache;
-} ;
+	char name[MAX_QPATH];
+	cache_user_t cache;
+};
 
 // !!! if this is changed, it much be changed in asm_i386.h too !!!
 struct sfxcache_t
 {
-	int 	length;
-	int 	loopstart;
-	int 	speed;
-	int 	width;
-	int 	stereo;
-	byte	data[1];		// variable sized
-} ;
+	int length;
+	int loopstart;
+	int speed;
+	int width;
+	int stereo;
+	byte data[1]; // variable sized
+};
 
 struct dma_t
 {
-	bool		gamealive;
-	bool		soundalive;
-	bool		splitbuffer;
-	int				channels;
-	int				samples;				// mono samples in buffer
-	int				submission_chunk;		// don't mix less than this #
-	int				samplepos;				// in mono samples
-	int				samplebits;
-	int				speed;
-	unsigned char	*buffer;
-} ;
+	bool gamealive;
+	bool soundalive;
+	bool splitbuffer;
+	int channels;
+	int samples; // mono samples in buffer
+	int submission_chunk; // don't mix less than this #
+	int samplepos; // in mono samples
+	int samplebits;
+	int speed;
+	unsigned char* buffer;
+};
 
 // !!! if this is changed, it much be changed in asm_i386.h too !!!
 struct channel_t
 {
-	sfx_t	*sfx;			// sfx number
-	int		leftvol;		// 0-255 volume
-	int		rightvol;		// 0-255 volume
-	int		end;			// end time in global paintsamples
-	int 	pos;			// sample position in sfx
-	int		looping;		// where to loop, -1 = no looping
-	int		entnum;			// to allow overriding a specific sound
-	int		entchannel;		//
-	vec3_t	origin;			// origin of sound effect
-	vec_t	dist_mult;		// distance multiplier (attenuation/clipK)
-	int		master_vol;		// 0-255 master volume
-} ;
+	sfx_t* sfx; // sfx number
+	int leftvol; // 0-255 volume
+	int rightvol; // 0-255 volume
+	int end; // end time in global paintsamples
+	int pos; // sample position in sfx
+	int looping; // where to loop, -1 = no looping
+	int entnum; // to allow overriding a specific sound
+	int entchannel; //
+	vec3_t origin; // origin of sound effect
+	vec_t dist_mult; // distance multiplier (attenuation/clipK)
+	int master_vol; // 0-255 master volume
+};
 
 struct wavinfo_t
 {
-	int		rate;
-	int		width;
-	int		channels;
-	int		loopstart;
-	int		samples;
-	int		dataofs;		// chunk starts this many bytes from file start
-} ;
+	int rate;
+	int width;
+	int channels;
+	int loopstart;
+	int samples;
+	int dataofs; // chunk starts this many bytes from file start
+};
 
 void S_Init();
 void S_Startup();
 void S_Shutdown();
-void S_StartSound (int entnum, int entchannel, sfx_t *sfx, vec3_t origin, float fvol,  float attenuation);
-void S_StaticSound (sfx_t *sfx, vec3_t origin, float vol, float attenuation);
-void S_StopSound (int entnum, int entchannel);
+void S_StartSound(int entnum, int entchannel, sfx_t* sfx, vec3_t origin, float fvol, float attenuation);
+void S_StaticSound(sfx_t* sfx, vec3_t origin, float vol, float attenuation);
+void S_StopSound(int entnum, int entchannel);
 void S_StopAllSounds(bool clear);
 void S_ClearBuffer();
-void S_Update (vec3_t origin, vec3_t v_forward, vec3_t v_right, vec3_t v_up);
+void S_Update(vec3_t origin, vec3_t v_forward, vec3_t v_right, vec3_t v_up);
 void S_ExtraUpdate();
 
-sfx_t *S_PrecacheSound (char *sample);
-void S_TouchSound (char *sample);
+sfx_t* S_PrecacheSound(char* sample);
+void S_TouchSound(char* sample);
 void S_ClearPrecache();
 void S_BeginPrecaching();
 void S_EndPrecaching();
@@ -110,10 +88,10 @@ void S_PaintChannels(int endtime);
 void S_InitPaintChannels();
 
 // picks a channel based on priorities, empty slots, number of channels
-channel_t *SND_PickChannel(int entnum, int entchannel);
+channel_t* SND_PickChannel(int entnum, int entchannel);
 
 // spatializes a channel
-void SND_Spatialize(channel_t *ch);
+void SND_Spatialize(channel_t* ch);
 
 // initializes cycling through a DMA buffer and returns information on it
 bool SNDDMA_Init();
@@ -132,12 +110,12 @@ void SNDDMA_Shutdown();
 #define	MAX_DYNAMIC_CHANNELS	128 //johnfitz -- was 8
 
 
-extern	channel_t   channels[MAX_CHANNELS];
+extern channel_t channels[MAX_CHANNELS];
 // 0 to MAX_DYNAMIC_CHANNELS-1	= normal entity sounds
 // MAX_DYNAMIC_CHANNELS to MAX_DYNAMIC_CHANNELS + NUM_AMBIENTS -1 = water, etc
 // MAX_DYNAMIC_CHANNELS + NUM_AMBIENTS to total_channels = static sounds
 
-extern	int			total_channels;
+extern int total_channels;
 
 //
 // Fake dma is a synchronous faking of the DMA progress used for
@@ -145,29 +123,29 @@ extern	int			total_channels;
 // number of times S_Update() is called per second.
 //
 
-extern bool 		fakedma;
-extern int 			fakedma_updates;
-extern int		paintedtime;
+extern bool fakedma;
+extern int fakedma_updates;
+extern int paintedtime;
 extern vec3_t listener_origin;
 extern vec3_t listener_forward;
 extern vec3_t listener_right;
 extern vec3_t listener_up;
-extern volatile dma_t *shm;
+extern volatile dma_t* shm;
 extern volatile dma_t sn;
 extern vec_t sound_nominal_clip_dist;
 
-extern	cvar_t loadas8bit;
-extern	cvar_t bgmvolume;
-extern	cvar_t volume;
+extern cvar_t loadas8bit;
+extern cvar_t bgmvolume;
+extern cvar_t volume;
 
-extern bool	snd_initialized;
+extern bool snd_initialized;
 
-extern int		snd_blocked;
+extern int snd_blocked;
 
-void S_LocalSound (char *s);
-sfxcache_t *S_LoadSound (sfx_t *s);
+void S_LocalSound(char* s);
+sfxcache_t* S_LoadSound(sfx_t* s);
 
-wavinfo_t GetWavinfo (char *name, byte *wav, int wavlength);
+wavinfo_t GetWavinfo(char* name, byte* wav, int wavlength);
 
 void SND_InitScaletable();
 void SNDDMA_Submit();
